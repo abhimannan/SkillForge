82523e6831ca3b69951a86d90fac48ce33b97c70
abhimannan <abhi543it@gmail.com>
2026-04-06 11:22:56 +0530

    Project deployement

---
+++ frontend/src/main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ClerkProvider } from "@clerk/clerk-react";
+import App from "./App";
+import "./index.css";
+
+const clerkPublishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+// PASTE YOUR CLERK PUBLISHABLE_KEY HERE
+// TO GET IT GO TO https://clerk.com/docs/react/getting-started/quickstart
+
+// THEN SIGNUP HERE
+// THEN YOU WILL GET YOUR KEY AFTER CREATING THE PROJECT.
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+ <ClerkProvider publishableKey={clerkPublishableKey}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ClerkProvider>
+);
